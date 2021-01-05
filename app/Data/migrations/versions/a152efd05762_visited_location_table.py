13b9af0c1846b522477c75feb0c75f31ce75f96c
Henrik Liman <Alpner93@gmail.com>
2021-01-05 13:21:44 +0100

    migration request fix

--- app/Data/migrations/versions/a152efd05762_visited_location_table.py
+++ app/Data/migrations/versions/a152efd05762_visited_location_table.py
@@ -21,7 +21,7 @@ depends_on = None
 def upgrade():
     op.create_table(
         'visited_location',
-        sa.Column('LocationId', sa.Integer, sa.ForeignKey('review.Id'), primary_key=True, nullable=False),
+        sa.Column('LocationId', sa.Integer, sa.ForeignKey('location.Id'), primary_key=True, nullable=False),
         sa.Column('UserId', sa.Integer, sa.ForeignKey('user.Id'), primary_key=True, nullable=False)
     )
 
