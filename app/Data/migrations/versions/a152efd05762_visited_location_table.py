852565525594b351ff8a6e02539f1d9b4cf6fc0a
Anton Maxén <Anton.maxen@gmail.com>
2021-01-05 11:55:20 +0100

    visited location migration done

--- app/Data/migrations/versions/a152efd05762_visited_location_table.py
+++ app/Data/migrations/versions/a152efd05762_visited_location_table.py
@@ -7,6 +7,7 @@ Create Date: 2021-01-04 14:22:12.103194
 """
 from alembic import op
 import sqlalchemy as sa
+from sqlalchemy.engine.reflection import Inspector
 
 
 # revision identifiers, used by Alembic.
@@ -17,8 +18,16 @@ depends_on = None
 
 
 def upgrade():
-    pass
+    op.create_table(
+        'visited_location',
+        sa.Column('LocationId', sa.Integer, sa.ForeignKey('location.Id'), nullable=False),
+        sa.Column('UserId', sa.Integer, sa.ForeignKey('user.Id'), nullable=False)
+    )
 
 
 def downgrade():
-    pass
+    conn = op.get_bind()
+    inspector = Inspector.from_engine(conn)
+    tables = inspector.get_table_names()
+    if 'visited_location' in tables:
+        op.drop_table('visited_location')
