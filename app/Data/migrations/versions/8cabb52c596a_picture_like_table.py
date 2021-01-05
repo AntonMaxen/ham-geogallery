7a453975fa4677eaacb23b9d2fe711c6e0410103
Henrik Liman <Alpner93@gmail.com>
2021-01-05 09:44:51 +0100

    picture_like(alembic) done

--- app/Data/migrations/versions/8cabb52c596a_picture_like_table.py
+++ app/Data/migrations/versions/8cabb52c596a_picture_like_table.py
@@ -8,8 +8,28 @@ Create Date: 2021-01-04 14:22:27.553965
 from alembic import op
 import sqlalchemy as sa
 
+# revision identifiers, used by Alembic.
+from sqlalchemy.engine.reflection import Inspector
+
+revision = '8cabb52c596a'
+down_revision = '387d4c07cc85'
+branch_labels = None
+depends_on = None
+
+
+"""picture_like_table
+
+Revision ID: 8cabb52c596a
+Revises: 387d4c07cc85
+Create Date: 2021-01-04 14:22:27.553965
+
+"""
+from alembic import op
+import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
+from sqlalchemy.engine.reflection import Inspector
+
 revision = '8cabb52c596a'
 down_revision = '387d4c07cc85'
 branch_labels = None
@@ -17,8 +37,18 @@ depends_on = None
 
 
 def upgrade():
-    pass
+    op.create_table(
+        'picture_like',
+        sa.Column('PictureId', sa.Integer, sa.ForeignKey('review.Id'), primary_key=True, nullable=False),
+        sa.Column('UserId', sa.Integer, sa.ForeignKey('user.Id'), primary_key=True, nullable=False),
+        sa.Column('Liked', sa.Boolean, nullable=False)
+    )
 
 
 def downgrade():
-    pass
+    conn = op.get_bind()
+    inspector = Inspector.from_engine(conn)
+    tables = inspector.get_table_names()
+    if 'picture_like' in tables:
+        op.drop_table('picture_like')
+
