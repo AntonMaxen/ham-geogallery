c9badf0966a1ba4b03517092df72e552a975d2ba
Henrik Liman <Alpner93@gmail.com>
2021-01-10 15:30:19 +0100

    Review_like table model

---
+++ app/Data/models/review_like.py
@@ -0,0 +1,14 @@
+from app.Data.db import Base
+import sqlalchemy as sa
+
+
+class Review_like(Base):
+    __tablename__ = 'review_like'
+
+    ReviewId = sa.Column(sa.Integer, sa.ForeignKey('review.Id'), nullable=False)
+    UserId = sa.Column(sa.Integer, sa.ForeignKey('user.Id'), nullable=False)
+    Liked = sa.Column(sa.Boolean, nullable=False)
+
+
+if __name__ == "__main__":
+    review_like = Review_like()
