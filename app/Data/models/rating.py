f51b7428c81c90345bfbe769846ae01695ef3f43
Anton Maxén <Anton.maxen@gmail.com>
2021-01-12 11:40:00 +0100

    rating relationships

--- app/Data/models/rating.py
+++ app/Data/models/rating.py
@@ -1,13 +1,26 @@
 from app.Data.db import Base
 import sqlalchemy as sa
+from sqlalchemy.orm import relationship
 
 
 class Rating(Base):
     __tablename__ = 'rating'
 
-    UserId = sa.Column(sa.Integer, sa.ForeignKey('user.Id'), nullable=False)
-    LocationId = sa.Column(sa.Integer, sa.ForeignKey('location.Id'), nullable=False)
-    Score = sa.Column(sa.Numeric(2, 1), nullable=False)
+    UserId = sa.Column(
+        sa.Integer,
+        sa.ForeignKey('user.Id', ondelete='CASCADE'),
+        nullable=False)
+    LocationId = sa.Column(
+        sa.Integer,
+        sa.ForeignKey('location.Id', ondelete='CASCADE'),
+        nullable=False)
+    Score = sa.Column(
+        sa.DECIMAL(2, 1),
+        nullable=False)
+
+    location = relationship('Location', back_populates='rating')
+    user = relationship('User', back_populates='rating')
+
 
 if __name__ == "__main__":
     rating = Rating()
