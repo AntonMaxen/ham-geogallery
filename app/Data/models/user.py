412221fc763217b11952cc28b6070053701d47bc
Henrik Liman <Alpner93@gmail.com>
2021-01-12 10:47:25 +0100

    User relationship

--- app/Data/models/user.py
+++ app/Data/models/user.py
@@ -1,3 +1,4 @@
+from sqlalchemy.orm import relationship
 from app.Data.db import Base
 import sqlalchemy as sa
 
@@ -5,18 +6,46 @@ import sqlalchemy as sa
 class User(Base):
     __tablename__ = 'user'
 
-    Id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
-    FirstName = sa.Column(sa.String(45), nullable=False)
-    LastName = sa.Column(sa.String(45), nullable=False)
-    Email = sa.Column(sa.String(45), nullable=False, unique=True)
-    Username = sa.Column(sa.String(45), nullable=False, unique=True)
-    Hash = sa.Column(sa.String(255), nullable=False)
-    Salt = sa.Column(sa.String(45), nullable=False)
-    PhoneNumber = sa.Column(sa.String(45), nullable=False, unique=True)
-    DateOfBirth = sa.Column(sa.Date, nullable=False)
-    JoinDate = sa.Column(sa.Date, nullable=False)
-    PermissionLevel = sa.Column(sa.Integer, nullable=False)
+    Id = sa.Column(
+        sa.Integer,
+        primary_key=True,
+        autoincrement=True)
+    FirstName = sa.Column(
+        sa.String(45),
+        nullable=False)
+    LastName = sa.Column(
+        sa.String(45),
+        nullable=False)
+    Email = sa.Column(
+        sa.String(45),
+        nullable=False,
+        unique=True)
+    Username = sa.Column(
+        sa.String(45),
+        nullable=False,
+        unique=True)
+    Hash = sa.Column(
+        sa.String(255),
+        nullable=False)
+    Salt = sa.Column(
+        sa.String(45),
+        nullable=False)
+    PhoneNumber = sa.Column(
+        sa.String(45),
+        nullable=False,
+        unique=True)
+    DateOfBirth = sa.Column(
+        sa.Date,
+        nullable=False)
+    JoinDate = sa.Column(
+        sa.Date,
+        nullable=False)
+    PermissionLevel = sa.Column(
+        sa.Integer,
+        nullable=False)
 
+    user_has_badge = relationship('UserBadge', back_populates='user')
+    location = relationship('Location', back_populates='user')
 
 if __name__ == '__main__':
     user = User()
