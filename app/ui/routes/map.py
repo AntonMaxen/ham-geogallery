3e878ecb7fe2c544f14da0e68ae72b3496d4045a
Anton Maxén <Anton.maxen@gmail.com>
2021-01-21 20:41:22 +0100

    map locations from db loaded in maps api and some functionality for maps js file

--- app/ui/routes/map.py
+++ app/ui/routes/map.py
@@ -1,12 +1,47 @@
-from flask import Blueprint, render_template, url_for
+from flask import Blueprint, render_template, url_for, request
 import app.bl.location_controller as lc
 import app.bl.utility_controller as uc
+import decimal
+import json
 bp = Blueprint('map', __name__, url_prefix='/map')
+
 
 @bp.route('/')
 def index():
-    return render_template('map.html')
+    locations = lc.get_all_locations()
+    location_dicts = uc.rows_to_dict(locations)
+    location_dicts = make_list_of_dicts_jsonable(location_dicts)
+    location_id = request.args.get('location_id')
+    location_id = location_id if location_id else None
+    jinja_dict = {
+        'locations': location_dicts,
+        'location_id': location_id
+    }
+
+    return render_template('map.html', **jinja_dict)
+
+
+@bp.route('/all_locations.json')
+def all_locations():
+    locations = lc.get_all_locations()
+    location_dicts = uc.rows_to_dict(locations)
+    location_dicts = make_list_of_dicts_jsonable(location_dicts)
+    return json.dumps(location_dicts)
+
+
+def make_list_of_dicts_jsonable(list_of_dicts):
+    return [make_dict_jsonable(my_dict) for my_dict in list_of_dicts]
+
+
+def make_dict_jsonable(my_dict):
+    return {k: float(str(v)) if isinstance(v, decimal.Decimal) else v
+            for k, v in my_dict.items()}
+
+
+def test():
+    location_dicts = uc.rows_to_dict(lc.get_all_locations())
+    print(make_list_of_dicts_jsonable(location_dicts))
 
 
 if __name__ == '__main__':
-    pass
+    test()
--- app/ui/static/js/map.js
+++ app/ui/static/js/map.js
@@ -3,12 +3,98 @@ let script = document.createElement('script');
 script.src = `https://maps.googleapis.com/maps/api/js?key=${api_key}&callback=initMap&libraries=&v=weekly`;
 script.defer = true;
 
+window.initMap = async () => {
+    const map = await create_map()
+    let home = map.getCenter()
+    place_current_location_marker(map, home)
+    let location_objects = load_locations(map, locations);
+    let focused_location = init_center_map(map, location_objects)
 
-window.initMap = () => {
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 11,
-    center: {lat: 62.323907, lng: -150.109291}
-  });
+
+}
+
+let create_map = async () => {
+    let center_lat_lng = new google.maps.LatLng(0, 0);
+    let position = await current_position()
+    if (position) {
+        center_lat_lng = new google.maps.LatLng(
+            position.coords.latitude,
+            position.coords.longitude
+        );
+    }
+    return new google.maps.Map(document.getElementById("map"), {
+        zoom: 11,
+        center: center_lat_lng
+    });
+}
+
+let current_position = async () => {
+    return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
+}
+
+let init_center_map = (map, location_objects) => {
+    let center_position = null;
+    if (location_id !== null && get_location_obj(location_objects, location_id)) {
+        let focused_location = get_location_obj(location_objects, location_id);
+        center_position = center_map_on_marker(map, focused_location.marker);
+    }
+    return center_position;
+}
+
+let place_current_location_marker = (map, latlng) => {
+    return new google.maps.Marker({
+        position: latlng,
+        map: map,
+        title: 'Current Location',
+        icon: '../static/images/Templatic-map-icons/residential-places.png'
+    });
+}
+
+let get_location_obj = (location_objects, location_id) => {
+    return location_objects.find(element => element.location.Id === location_id);
+}
+
+let center_map_on_geopos = async (map) => {
+    let current_pos = await current_position();
+    let latlng = new google.maps.LatLng(current_pos.coords.latitude, current_pos.coords.longitude);
+    map.setCenter(latlng);
+    return latlng;
+}
+
+let center_map_on_marker = (map, marker) => {
+    let position = marker.getPosition();
+    map.setCenter(position);
+    return position;
+}
+
+
+let load_locations = (map, locations) => {
+    let location_objects = [];
+    locations.forEach(l => {
+        let location_object = {}
+        let title_text = `Place: ${l.Place}\n`+
+            `Name: ${l.Name}\n` +
+            `Longitude: ${l.Longitude}\n` +
+            `Latitude: ${l.Latitude}\n`;
+
+        location_object.marker = new google.maps.Marker({
+            position: {lat: l.Latitude, lng: l.Longitude},
+            map,
+            title: title_text
+        });
+        location_object.location = l;
+        location_objects.push(location_object);
+    });
+
+    return location_objects;
+}
+
+
+let get_random_item = arr => {
+    let random_index = Math.floor(Math.random() * arr.length);
+    return arr[random_index];
 }
 
 document.head.appendChild(script);
