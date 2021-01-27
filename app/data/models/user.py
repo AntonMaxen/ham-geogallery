67a8b32c06b47cfe5bfb29a0fb0fdd71eb66f4f0
Mattias Barth <3858736+mattiasbarth@users.noreply.github.com>
2021-01-27 16:09:38 +0100

    whip

--- app/data/models/user.py
+++ app/data/models/user.py
@@ -6,50 +6,43 @@ from sqlalchemy.orm import relationship
 class User(Base):
     __tablename__ = 'user'
 
-    Id = sa.Column(
+    id = sa.Column(
         sa.Integer,
         primary_key=True,
         autoincrement=True
     )
-    FirstName = sa.Column(
-        sa.String(45),
-        nullable=False
+    first_name = sa.Column(
+        sa.String(45)
     )
-    LastName = sa.Column(
-        sa.String(45),
-        nullable=False
+    last_name = sa.Column(
+        sa.String(45)
     )
-    Email = sa.Column(
+    email = sa.Column(
         sa.String(45),
         nullable=False,
         unique=True
     )
-    Username = sa.Column(
+    username = sa.Column(
         sa.String(45),
         nullable=False,
         unique=True
     )
-    Hash = sa.Column(
+    hashed_password = sa.Column(
         sa.String(255),
         nullable=False
     )
-    Salt = sa.Column(
-        sa.String(255),
-        nullable=False
-    )
-    PhoneNumber = sa.Column(
+    phone_number = sa.Column(
         sa.String(45),
-        nullable=False,
         unique=True
     )
-    DateOfBirth = sa.Column(
+    date_of_birth = sa.Column(
         sa.Date
     )
-    JoinDate = sa.Column(
+    date_created = sa.Column(
         sa.Date,
         nullable=False
     )
-    PermissionLevel = sa.Column(
+    permission_level = sa.Column(
         sa.Integer,
         nullable=False
     )
