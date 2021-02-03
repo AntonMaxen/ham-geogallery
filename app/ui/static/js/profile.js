8a0af77a29bfad1cfafb783e47af7e0ae3236c2e
Anton Maxén <Anton.maxen@gmail.com>
2021-02-03 17:47:59 +0100

    added token to user for authorized endpoints on api and added the authenticated to profile view and some js

--- app/ui/static/js/profile.js
+++ app/ui/static/js/profile.js
@@ -1,34 +1,51 @@
+import {create_alert, fade_out_element} from "./functions.js";
+
 window.onload = async () => {
     let user_form = document.querySelector('#user-form');
     user_form.addEventListener('submit', async function(e) {
         e.preventDefault()
-        let user_id = this.querySelector('input[name="user_id"]').value;
-        let first_name = this.querySelector('input[name="first_name"]').value;
-        let last_name = this.querySelector('input[name="last_name"]').value;
-        let email = this.querySelector('input[name="email"]').value;
-        let username = this.querySelector('input[name="username"]').value;
-        let phone = this.querySelector('input[name="phone"]').value;
-        let date_of_birth = this.querySelector('input[name="date_of_birth"]').value;
-        console.log(first_name);
-        console.log(last_name);
-        console.log(email);
-        console.log(username);
-        console.log(phone);
-        console.log(date_of_birth);
+        let token = this.querySelector('input[name="token"]');
+        let user_id = this.querySelector('input[name="user_id"]');
+        let first_name = this.querySelector('input[name="first_name"]');
+        let last_name = this.querySelector('input[name="last_name"]');
+        let email = this.querySelector('input[name="email"]');
+        let username = this.querySelector('input[name="username"]');
+        let phone = this.querySelector('input[name="phone"]');
+        let date_of_birth = this.querySelector('input[name="date_of_birth"]');
+
         let form_data = new FormData();
-        form_data.append('user_id', user_id);
-        form_data.append('first_name', first_name);
-        form_data.append('last_name', last_name);
-        form_data.append('email', email);
-        form_data.append('username', username);
-        form_data.append('phone', phone);
-        form_data.append('date_of_birth', date_of_birth);
+        form_data.append('token', token.value);
+        form_data.append('user_id', user_id.value);
+        form_data.append('first_name', first_name.value);
+        form_data.append('last_name', last_name.value);
+        form_data.append('email', email.value);
+        form_data.append('username', username.value);
+        form_data.append('phone', phone.value);
+        form_data.append('date_of_birth', date_of_birth.value);
         let result = await fetch("http://localhost:5000/api/update/user", {
             method: "POST",
             body: form_data
         });
 
-        location.reload();
+        let data = await result.json();
+        console.log(data);
+        if (data.status == 200) {
+            let user = data.user;
+            first_name.value = user.FirstName;
+            last_name.value = user.LastName;
+            email.value = user.Email;
+            username.value = user.Username;
+            phone.value = user.PhoneNumber;
+            date_of_birth.value = user.DateOfBirth;
+            let header = document.querySelector('.container > .header');
+            header.innerText = `Profile for ${user.Username}`;
+            let alert = create_alert(data.category, data.message);
+            fade_out_element(alert);
+
+        } else {
+            let alert = create_alert(data.category, data.message);
+            fade_out_element(alert);
+        }
 
     });
 }
